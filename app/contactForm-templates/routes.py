6c27d9f8143ab4c7bf29140cf9452328175ef3bb
Thegoldenjewls <Julianlucoronado@gmail.com>
2022-07-10 08:42:01 -0700

    final

--- app/contactForm-templates/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-contactForm = Blueprint('contactForm', __name__, template_folder = 'contactForm-templates')
-
-@contactForm.route('/contactForm', methods = ['POST'])
-def enterInfo():
-    return render_template('contactForm.html')
--- run.py
+++ run.py
@@ -1,26 +1,10 @@
-#     You need the recreate the following:
-# Create a virtural Environment 
-# Install requirements in the environment (flask, python-dotenv, etc.)
-# Install Flask-WTF (Form Validation)
-# Create the basic file structure for a flask package (app folder -> __init__, routes, forms, etc.)
-# Create a home route for the home page
-# Create the "Register Phone Number" route
-#     - Display a form that will take in the following information:
-#          - Name
-#          - Phone Number
-#          - Address
-#     - On form submit, print out the user info 
-#          * For added bonus, Flash a message with the new user info
+from app import app, db
+from app.models import User, Post
 
-# Links for reference
-# Flask Documentation
-# Flask-WTF
+@app.shell_context_processor
+def make_context():
+    return {'db': db, 'Post': Post, 'User': User}
 
-# GitHub repository to Kekambas Flask
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.before_first_request
+def create_tables():
+    db.create_all()
